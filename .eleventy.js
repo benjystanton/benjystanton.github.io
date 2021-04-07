800b36c09a3670dcfab28836289533672bfa77cf
Benjy Stanton <benjystanton@gmail.com>
2021-04-07 22:06:51 +0100

    #108

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function (eleventyConfig) {
     // Set directories to pass through to the dist folder
     eleventyConfig.addPassthroughCopy('./src/images/');
     eleventyConfig.addPassthroughCopy('./.htaccess');
+    eleventyConfig.addPlugin(pluginRss);
 
     return {
         dir: {
